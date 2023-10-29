c82b7e9a8be3ef3c075457a2a77839d1df3945da
Hank_TUF <hck134599@gmail.com>
2023-10-29 16:42:50 +0800

    Read and resize picture

--- hw6-1_10_27/Form1.Designer.cs
+++ hw6-1_10_27/Form1.Designer.cs
@@ -1,7 +1,5 @@
-﻿namespace hw6_1_10_27
-{
-    partial class Form1
-    {
+﻿namespace hw6_1_10_27 {
+    partial class Form1 {
         /// <summary>
         ///  Required designer variable.
         /// </summary>
@@ -11,10 +9,8 @@
         ///  Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing && (components != null))
-            {
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
                 components.Dispose();
             }
             base.Dispose(disposing);
@@ -26,27 +22,23 @@
         ///  Required method for Designer support - do not modify
         ///  the contents of this method with the code editor.
         /// </summary>
-        private void InitializeComponent()
-        {
-            OFD = new OpenFileDialog();
+        private void InitializeComponent() {
             label1 = new Label();
             label2 = new Label();
-            button1 = new Button();
-            button2 = new Button();
-            trackBar1 = new TrackBar();
-            pictureBox1 = new PictureBox();
-            ((System.ComponentModel.ISupportInitialize)trackBar1).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            DrawBtn = new Button();
+            ChooseBtn = new Button();
+            Bar = new TrackBar();
+            PicBox = new PictureBox();
+            ShowLabel2 = new Label();
+            ShowLabel1 = new Label();
+            ((System.ComponentModel.ISupportInitialize)Bar).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)PicBox).BeginInit();
             SuspendLayout();
-            // 
-            // OFD
-            // 
-            OFD.FileName = "openFileDialog1";
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(491, 81);
+            label1.Location = new Point(433, 86);
             label1.Name = "label1";
             label1.Size = new Size(42, 15);
             label1.TabIndex = 0;
@@ -55,72 +47,101 @@
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(496, 128);
+            label2.Location = new Point(438, 133);
             label2.Name = "label2";
             label2.Size = new Size(42, 15);
             label2.TabIndex = 1;
             label2.Text = "label2";
             // 
-            // button1
+            // DrawBtn
             // 
-            button1.Location = new Point(162, 430);
-            button1.Name = "button1";
-            button1.Size = new Size(75, 23);
-            button1.TabIndex = 2;
-            button1.Text = "button1";
-            button1.UseVisualStyleBackColor = true;
+            DrawBtn.Font = new Font("Microsoft JhengHei UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            DrawBtn.Location = new Point(114, 425);
+            DrawBtn.Name = "DrawBtn";
+            DrawBtn.Size = new Size(200, 35);
+            DrawBtn.TabIndex = 2;
+            DrawBtn.Text = "繪製拼圖板";
+            DrawBtn.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // ChooseBtn
             // 
-            button2.Location = new Point(790, 433);
-            button2.Name = "button2";
-            button2.Size = new Size(75, 23);
-            button2.TabIndex = 3;
-            button2.Text = "button2";
-            button2.UseVisualStyleBackColor = true;
+            ChooseBtn.Font = new Font("Microsoft JhengHei UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            ChooseBtn.Location = new Point(594, 425);
+            ChooseBtn.Name = "ChooseBtn";
+            ChooseBtn.Size = new Size(200, 35);
+            ChooseBtn.TabIndex = 3;
+            ChooseBtn.Text = "選擇圖片";
+            ChooseBtn.UseVisualStyleBackColor = true;
+            ChooseBtn.Click += ChooseBtn_Click;
             // 
-            // trackBar1
+            // Bar
             // 
-            trackBar1.Location = new Point(776, 372);
-            trackBar1.Name = "trackBar1";
-            trackBar1.Size = new Size(104, 45);
-            trackBar1.TabIndex = 4;
+            Bar.LargeChange = 1;
+            Bar.Location = new Point(626, 374);
+            Bar.Maximum = 1;
+            Bar.Name = "Bar";
+            Bar.Size = new Size(151, 45);
+            Bar.TabIndex = 4;
+            Bar.Value = 1;
+            Bar.ValueChanged += Bar_ValueChanged;
             // 
-            // pictureBox1
+            // PicBox
             // 
-            pictureBox1.Location = new Point(718, 116);
-            pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(100, 50);
-            pictureBox1.TabIndex = 5;
-            pictureBox1.TabStop = false;
+            PicBox.Location = new Point(556, 86);
+            PicBox.Name = "PicBox";
+            PicBox.Size = new Size(270, 270);
+            PicBox.TabIndex = 5;
+            PicBox.TabStop = false;
+            // 
+            // ShowLabel2
+            // 
+            ShowLabel2.AutoSize = true;
+            ShowLabel2.Font = new Font("Microsoft JhengHei UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            ShowLabel2.Location = new Point(783, 374);
+            ShowLabel2.Name = "ShowLabel2";
+            ShowLabel2.Size = new Size(41, 20);
+            ShowLabel2.TabIndex = 8;
+            ShowLabel2.Text = "顯示";
+            // 
+            // ShowLabel1
+            // 
+            ShowLabel1.AutoSize = true;
+            ShowLabel1.Font = new Font("Microsoft JhengHei UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            ShowLabel1.Location = new Point(563, 374);
+            ShowLabel1.Name = "ShowLabel1";
+            ShowLabel1.Size = new Size(57, 20);
+            ShowLabel1.TabIndex = 9;
+            ShowLabel1.Text = "不顯示";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(984, 511);
-            Controls.Add(pictureBox1);
-            Controls.Add(trackBar1);
-            Controls.Add(button2);
-            Controls.Add(button1);
+            ClientSize = new Size(894, 511);
+            Controls.Add(ShowLabel1);
+            Controls.Add(ShowLabel2);
+            Controls.Add(PicBox);
+            Controls.Add(Bar);
+            Controls.Add(ChooseBtn);
+            Controls.Add(DrawBtn);
             Controls.Add(label2);
             Controls.Add(label1);
             Name = "Form1";
             Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)trackBar1).EndInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)Bar).EndInit();
+            ((System.ComponentModel.ISupportInitialize)PicBox).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
-
-        private OpenFileDialog OFD;
         private Label label1;
         private Label label2;
-        private Button button1;
-        private Button button2;
-        private TrackBar trackBar1;
-        private PictureBox pictureBox1;
+        private Button DrawBtn;
+        private Button ChooseBtn;
+        private TrackBar Bar;
+        private PictureBox PicBox;
+        private Label ShowLabel2;
+        private Label ShowLabel1;
     }
 }
--- hw6-1_10_27/Form1.cs
+++ hw6-1_10_27/Form1.cs
@@ -1,10 +1,53 @@
-namespace hw6_1_10_27
-{
-    public partial class Form1 : Form
-    {
-        public Form1()
-        {
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+
+namespace hw6_1_10_27 {
+    public partial class Form1 : Form {
+        Image? img;
+
+        public Form1() {
             InitializeComponent();
+        }
+
+        public static Bitmap Resize_img(Image image, int width, int height) {
+
+            var destRect = new Rectangle(0, 0, width, height);
+            var destImage = new Bitmap(width, height);
+
+            destImage.SetResolution(image.HorizontalResolution, image.VerticalResolution);
+
+            using (var graphics = Graphics.FromImage(destImage)) {
+                graphics.CompositingMode = CompositingMode.SourceCopy;
+                graphics.CompositingQuality = CompositingQuality.HighQuality;
+                graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                graphics.SmoothingMode = SmoothingMode.HighQuality;
+                graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
+                using ImageAttributes wrapMode = new();
+                wrapMode.SetWrapMode(WrapMode.TileFlipXY);
+                graphics.DrawImage(image, destRect, 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, wrapMode);
+            }
+
+            return destImage;
+        }
+
+        private void ChooseBtn_Click(object sender, EventArgs e) {
+            using OpenFileDialog OFD = new();
+            OFD.Filter = "圖片檔案 (*.jpg;*.jpeg;*.png;*.bmp;*.gif)|*.jpg;*.jpeg;*.png;*.bmp;*.gif|所有檔案 (*.*)|*.*";
+            OFD.Title = "選擇圖片";
+            OFD.Multiselect = false;
+            OFD.InitialDirectory = ".";
+
+            if (OFD.ShowDialog() == DialogResult.OK) {
+                string path = OFD.FileName;
+                img = Resize_img(Image.FromFile(path), 270, 270);
+
+                PicBox.Image = img;
+            }
+        }
+
+        private void Bar_ValueChanged(object sender, EventArgs e) {
+            PicBox.Visible = Bar.Value == 1;
         }
     }
 }
